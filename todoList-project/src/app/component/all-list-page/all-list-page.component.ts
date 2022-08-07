6e77dec93676fe8c5b6b7398eb0c0ace376942b7
yuval291 <yuval291195@gmail.com>
2022-08-07 09:08:23 +0300

    todo list project

---
+++ todoList-project/src/app/component/all-list-page/all-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-list-page',
+  templateUrl: './all-list-page.component.html',
+  styleUrls: ['./all-list-page.component.css']
+})
+export class AllListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
